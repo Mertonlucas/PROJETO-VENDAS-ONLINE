5ecb71b434a3ff3448b8c2fbb5249f7652d192b9
Lucas Merton <126838077+Mertonlucas@users.noreply.github.com>
2023-11-15 23:42:58 -0300

    Update

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -5,3 +5,25 @@ export interface CreateUserDto {
   cpf: string;
   password: string;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// define uma interface em TypeScript chamada `CreateUserDto`. A interface especifica que um 
+// objeto do tipo `CreateUserDto` deve ter as propriedades `name`, `email`, `phone`, `cpf` e `password`, 
+// todas do tipo `string`.
--- src/user/interface/user.entity.ts
+++ src/user/interface/user.entity.ts
@@ -31,3 +31,20 @@ export class UserEntity {
     @UpdateDateColumn()
     updatedat: Date;
 }
+
+
+
+
+
+
+
+
+
+// Uma interface descreve um conjunto de assinaturas de métodos, ou seja, os nomes, os parâmetros e os 
+// tipos de retorno dos métodos que uma classe pode implementar. <<
+// No caso do user entity, a interface define uma entidade do TypeORM chamada UserEntity, 
+// que >> mapeia uma tabela do banco de dados chamada user. A entidade tem os seguintes atributos: id, name, email, 
+// phone, cpf, password, typeUser, createdat e updatedat. Cada atributo tem um tipo de dado, uma restrição de 
+// nulidade e um decorador que indica como o atributo é mapeado para o banco de dados. A interface não define 
+// como os atributos são armazenados, consultados ou modificados no banco de dados, apenas define a estrutura 
+// da entidade.
