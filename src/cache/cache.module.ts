128f79075e13821362fcbb6d8fb04db6c4d0734c
Lucas Merton <126838077+Mertonlucas@users.noreply.github.com>
2023-12-05 23:49:09 -0300

    [UPDATED SERVICE + CACHEMODULE + CITYMODULE]
    "Implementação da função 'getAllCitiesByStateId' no serviço 'CityService' do Nest.js. Esta função busca todas as cidades associadas a um estado específico no banco de dados, otimizando o desempenho do sistema por meio do uso de cache fornecido pelo serviço 'CacheService'. As alterações incluem a introdução da funcionalidade de cache para melhorar a eficiência da busca de cidades por estado."

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
+import { CacheModule as CacheManagerModule } from '@nestjs/cache-manager';
 
 @Module({
-  providers: [CacheService]
+  imports: [
+    CacheManagerModule.register({
+      ttl: 900000000,
+    }),
+  ],
+
+  providers: [CacheService],
+  exports: [CacheService],
 })
 export class CacheModule {}
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,38 +1,29 @@
 // O algoritmo em questão é uma função chamada "getAllCitiesByStateId". Ela busca todas as 
 // cidades associadas a um estado específico no banco de dados através do cache que ajuda no desempenho do sistema.
 
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
+import { CacheService } from 'src/cache/cache.service';
 
 @Injectable()
-
 export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+
+    private readonly cacheService: CacheService,
   ) {}
 
   async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
-    const citiesCache: CityEntity[] = await this.cacheManager.get(
-      `state_${stateId}`,
+    return this.cacheService.getCache<CityEntity[]>(`state_${stateId}`, () =>
+      this.cityRepository.find({
+        where: {
+          stateId,
+        },
+      })
     );
-
-    if (citiesCache) {
-      return citiesCache;
-    }
-
-    const cities = await this.cityRepository.find({
-      where: {
-        stateId,
-      },
-    });
-    await this.cacheManager.set(`state_${stateId}`, cities); 
-
-    return cities;
   }
 }
+ 
