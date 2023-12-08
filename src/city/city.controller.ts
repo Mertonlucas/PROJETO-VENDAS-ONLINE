d25a0ea41a604fed1f8d63a9b7aa658cb5d01d71
Lucas Merton <126838077+Mertonlucas@users.noreply.github.com>
2023-12-07 23:30:09 -0300

    Update city.controller.ts

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -13,3 +13,12 @@ export class CityController {
         return this.cityService.getAllCitiesByStateId(stateId);
     }
 }
+
+
+// no contexto do Nest.js (ou de muitos outros frameworks MVC), as solicitações do cliente 
+// passam primeiro pelos controllers. O controller atua como um intermediário entre as solicitações HTTP do cliente 
+// e a lógica de negócios do aplicativo.
+// Quando o cliente faz uma solicitação relacionada à entidade "city", e;
+// ssa solicitação é roteada para o controller correspondente. O controller processa a solicitação, 
+// interage com os serviços apropriados (por exemplo, serviços que lidam com a lógica de negócios relacionada 
+//     à cidade) e, em seguida, retorna uma resposta adequada ao cliente.
