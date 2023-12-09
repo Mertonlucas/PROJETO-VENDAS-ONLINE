1ea54c000b0d402ec8fac7a068d98fd33e6c4104
Lucas Merton <126838077+Mertonlucas@users.noreply.github.com>
2023-12-09 10:08:27 -0300

    [UPDATED] LOCK-JSON

---
+++ src/address/address.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+
+import { AddressService } from './address.service';
+import { CreateAddressDto } from './dtos/createAddress.dto';
+import { AddressEntity } from './entities/adress.entity';
+
+@Controller('address')
+export class AddressController {
+  constructor(private readonly addressService: AddressService) {}
+
+  @Post(':userId')  // Adicionando ':userId' como parte do caminho
+  @UsePipes(ValidationPipe)
+  async createAddress(
+    @Body() createAddressDto: CreateAddressDto,
+    @Param('userId') userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressService.createAddress(createAddressDto, userId);
+  }
+}
