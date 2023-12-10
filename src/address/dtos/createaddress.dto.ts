dd269d5c6738f229516690ef2f56d9ea05b58737
Lucas Merton <126838077+Mertonlucas@users.noreply.github.com>
2023-12-10 17:11:44 -0300

    Update createaddress.dto.ts

--- src/address/dtos/createaddress.dto.ts
+++ src/address/dtos/createaddress.dto.ts
@@ -4,13 +4,10 @@ export class CreateAddressDto {
   @IsString()
   @IsOptional()
   complement: string;
-
   @IsInt()
   numberAddress: number;
-
   @IsString()
   cep: string;
-
   @IsInt()
   cityId: number;
 }
