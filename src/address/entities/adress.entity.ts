beba962a8449005fa75ffedf227aa32f3fce570c
Lucas Merton <126838077+Mertonlucas@users.noreply.github.com>
2023-12-12 15:41:50 -0300

    Update adress.entity.ts

--- src/address/entities/adress.entity.ts
+++ src/address/entities/adress.entity.ts
@@ -9,28 +9,28 @@ import {
 @Entity({ name: 'address' })
 export class AddressEntity {
   @PrimaryGeneratedColumn('rowid')
-  id: number;
+    id: number;
 
   @Column({ name: 'user_id', nullable: false })
-  userId: number;
+    userId: number;
 
   @Column({ name: 'complement', nullable: true })
-  complement: string;
+    complement: string;
 
   @Column({ name: 'number', nullable: false })
-  numberAddress: number;
+    numberAddress: number;
 
   @Column({ name: 'cep', nullable: false })
-  cep: string;
+    cep: string;
 
   @Column({ name: 'city_id', nullable: false })
-  cityId: number;
+    cityId: number;
 
   @CreateDateColumn({ name: 'created_at' })
-  createdAt: Date;
+    createdAt: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt: Date;
+   updatedAt: Date;
 }
 
 
