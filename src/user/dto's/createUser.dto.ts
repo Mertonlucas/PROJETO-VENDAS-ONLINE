71f0af6d69ab088072a2c4bc5d8a46061a2d0993
Lucas Merton <126838077+Mertonlucas@users.noreply.github.com>
2023-12-18 20:23:48 -0300

    Create createUser.dto.ts

---
+++ src/user/dto's/createUser.dto.ts
@@ -0,0 +1,22 @@
+import { IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  phone: string;
+
+  @IsString()
+  cpf: string;
+
+  @IsString()
+  password: string;
+}
+
+// define uma interface em TypeScript chamada `CreateUserDto`. A interface especifica que um
+// objeto do tipo `CreateUserDto` deve ter as propriedades `name`, `email`, `phone`, `cpf` e `password`,
+// todas do tipo `string`.
