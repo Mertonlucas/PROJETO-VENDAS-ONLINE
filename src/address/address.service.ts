51946078f94e5776ca7682c000206bd0b33b7d97
Lucas Merton <126838077+Mertonlucas@users.noreply.github.com>
2023-12-19 20:49:46 -0300

    Update address.service.ts

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -3,21 +3,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressEntity } from './entities/adress.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AddressService {
   constructor(
     @InjectRepository(AddressEntity)
     private readonly addressRepository: Repository<AddressEntity>,
+    private readonly userService: UserService
   ) {}
 
   async createAddress(
     createAddressDto: CreateAddressDto,
     userId: number,
   ): Promise<AddressEntity> {
+    const user = await this.userService.findUserById(userId);
     return this.addressRepository.save({
       ...createAddressDto,
-      userId,
+      userId: user.id,
     });
   }
 }
